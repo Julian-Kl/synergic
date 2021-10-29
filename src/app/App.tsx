72d035c657eb559b8d415e32f80ad5697d85eca1
Julian-Kl <julianklummer@outlook.de>
2021-10-29 23:27:12 +0200

    first commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './styles/app.scss'
 import { Router } from './router/Router'
 
 export const App: React.FC = () => {
--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import routes from './routes'
 import { Route, Switch } from 'react-router-dom'
 import { NoMatch } from '../pages/404/NoMatch'
-import { Navbar } from '../components/organisms/Navbar/Navbar'
 
 export const Router: React.FC = () => {
     return (
         <>
-            <Navbar routes={routes} />
             <Switch>
                 {routes.map(({ path, exact, component }, key) => (
                     <Route key={key} path={path} exact={exact}>
