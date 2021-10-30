c3523ee37379669fc902805522e02d9379b271c8
Julian-Kl <julianklummer@outlook.de>
2021-10-30 12:25:30 +0200

    first grid test

--- src/app/pages/start/Start.tsx
+++ src/app/pages/start/Start.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 import ExampleImage from '../../assets/images/ExampleImage.png'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    boxShadow: 'none',
+    border: 'solid 1px darkblue'
+  }));
 
 export const Start: React.FC = () => {
     return (
@@ -12,6 +25,24 @@ export const Start: React.FC = () => {
             <p>Homepage</p>
             <img src={ExampleImage}></img>
             <p>Willkommen</p>
+            <div style={{ maxWidth: 500}}>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <Item>xs=8</Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Item>xs=8</Item>
+                    </Grid>
+                </Grid>
+            </Box>
+            </div>
         </>
     )
 }
