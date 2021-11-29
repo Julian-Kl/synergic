2623aeda6547c92baa021bcb1e40dc0ae3d95264
Julian-Kl <40470983+Julian-Kl@users.noreply.github.com>
2021-11-29 17:28:26 +0100

    useless but nice startpage added

--- src/app/pages/start/Start.tsx
+++ src/app/pages/start/Start.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material'
+import { Grow, Typography } from '@mui/material'
+import ParticlesBg from 'particles-bg'
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 
@@ -6,23 +7,22 @@ export const Start: React.FC = () => {
     return (
         <>
             <Helmet>
-                <title>Home</title>
+                <title>Synergic</title>
                 <meta name='description' content='This is Home' />
             </Helmet>
-            <Typography
-                variant='h1'
-                component='h1'
-                gutterBottom
-            >
-                Synergic
-            </Typography>
-            <Typography
-                variant='h2'
-                component='h2'
-                gutterBottom
-            >
-                Atomic Design Page Builder
-            </Typography>
+            <ParticlesBg type='cobweb' color='#1976d2' bg />
+            <div style={{ position: 'absolute', top: 200, left: 80 }}>
+                <Grow in {...{ timeout: 1000 }}>
+                    <Typography variant='h1' component='h1'>
+                        Synergic
+                    </Typography>
+                </Grow>
+                <Grow in {...{ timeout: 2000 }}>
+                    <Typography variant='h2' component='h2'>
+                        Atomic Design Page Builder
+                    </Typography>
+                </Grow>
+            </div>
         </>
     )
 }
--- src/website/App.tsx
+++ src/website/App.tsx
@@ -1,13 +1,49 @@
 import { CssBaseline } from '@mui/material'
-import React from 'react'
-import { HelmetProvider } from 'react-helmet-async'
-import { PageRouter } from './pageRouter/PageRouter'
+import React, { useEffect, useState } from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { Route, Switch, useRouteMatch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import { LoadingBackdrop } from '../app/components/atoms/LoadingBackdrop/LoadingBackdrop'
+import { contentApiUrl } from '../app/services/contentApiUrl'
+import { fetchApi } from '../app/services/fetchApi'
+import { PageData } from '../app/types/PageData'
+import { Page } from './pageRender/Page'
+
 export const App: React.FC = () => {
+    const [pages, setPages] = useState<PageData[]>([])
+    const [loading, setLoading] = useState(true)
+    const { path, url } = useRouteMatch()
+
+    const loadData = async () => {
+        const response = await fetchApi(`${contentApiUrl}/pages`)
+        setPages(response.data)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        loadData()
+    }, [])
 
     return (
         <HelmetProvider>
             <CssBaseline />
-            <PageRouter />
+            <BrowserRouter>
+                {loading && <LoadingBackdrop />}
+                <Switch>
+                    {pages.map((page, key) => (
+                        <Route key={key} path={`${path}/${page.route}`} exact>
+                            <Helmet>
+                                <title>{page.title}</title>
+                                <meta
+                                    name='description'
+                                    content='This is the Page Editor'
+                                />
+                            </Helmet>
+                            <Page page={page} />
+                        </Route>
+                    ))}
+                </Switch>
+            </BrowserRouter>
         </HelmetProvider>
     )
 }
