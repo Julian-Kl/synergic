560887e086e9dfcaf8b118b519583ee714e07fb4
Julian-Kl <40470983+Julian-Kl@users.noreply.github.com>
2021-11-29 16:58:43 +0100

    preview page navigation refactored

--- src/website/pageRouter/PageRouter.tsx
+++ src/website/pageRouter/PageRouter.tsx
@@ -1,19 +1,13 @@
-import {
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
-    Typography
-} from '@mui/material'
-import { Box } from '@mui/system'
 import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { Route, Switch, useLocation, useRouteMatch } from 'react-router'
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { LoadingBackdrop } from '../../app/components/atoms/LoadingBackdrop/LoadingBackdrop'
 import { contentApiUrl } from '../../app/services/contentApiUrl'
 import { fetchApi } from '../../app/services/fetchApi'
 import { PageData } from '../../app/types/PageData'
 import { Page } from '../pageRender/Page'
+import { PageRouterNavigation } from './PageRouterNavigation'
 
 export const PageRouter: React.FC = () => {
     const [pages, setPages] = useState<PageData[]>([])
@@ -43,47 +37,15 @@ export const PageRouter: React.FC = () => {
     }, [])
 
     return (
+        
         <BrowserRouter>
-            <Box sx={{ width: '100%', backgroundColor: 'lightgray' }}>
-                <Box sx={{ maxWidth: 240, marginLeft: 2 }}>
-                    <FormControl fullWidth>
-                        <InputLabel id='demo-simple-select-label'>
-                            Page
-                        </InputLabel>
-                        <Select
-                            labelId='demo-simple-select-label'
-                            id='demo-simple-select'
-                            value={pageName}
-                            label='Page'
-                            onChange={(e) => setPageName(e.target.value)}
-                        >
-                            {pages.map(({ name, route }, key) => (
-                                <MenuItem
-                                    value={name}
-                                    key={key}
-                                    style={{ padding: 0 }}
-                                >
-                                    <Link
-                                        to={`${url}/${route}`}
-                                        style={{
-                                            width: '100%',
-                                            padding: '6px 16px 6px 16px',
-                                        }}
-                                    >
-                                        <Typography
-                                            variant='body1'
-                                            component='span'
-                                        >
-                                            {name}
-                                        </Typography>
-                                    </Link>
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Box>
-            </Box>
-
+        {loading && <LoadingBackdrop />}
+            <PageRouterNavigation 
+                pageName={pageName}
+                setPageName= {setPageName}
+                pages={pages}
+                url={url}
+            />
             <Switch>
                 {pages.map((page, key) => (
                     <Route key={key} path={`${path}/${page.route}`} exact>
---
+++ src/website/pageRouter/PageRouterNavigation.tsx
@@ -0,0 +1,58 @@
+
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { PageData } from '../../app/types/PageData'
+
+interface Props {
+    pageName: string
+    setPageName: (name: string) => void
+    pages: PageData[]
+    url: string
+}
+
+export const PageRouterNavigation: React.FC<Props> = (props: Props) => {
+    
+    return (
+        
+            <Box sx={{ width: '100%', backgroundColor: 'lightgray' }}>
+                <Box sx={{ maxWidth: 240, marginLeft: 2 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id='demo-simple-select-label'>
+                            Page
+                        </InputLabel>
+                        <Select
+                            labelId='demo-simple-select-label'
+                            id='demo-simple-select'
+                            value={props.pageName}
+                            label='Page'
+                            onChange={(e) => props.setPageName(e.target.value)}
+                        >
+                            {props.pages.map(({ name, route }, key) => (
+                                <MenuItem
+                                    value={name}
+                                    key={key}
+                                    style={{ padding: 0 }}
+                                >
+                                    <Link
+                                        to={`${props.url}/${route}`}
+                                        style={{
+                                            width: '100%',
+                                            padding: '6px 16px 6px 16px',
+                                        }}
+                                    >
+                                        <Typography
+                                            variant='body1'
+                                            component='span'
+                                        >
+                                            {name}
+                                        </Typography>
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
+            </Box>
+    )
+}
