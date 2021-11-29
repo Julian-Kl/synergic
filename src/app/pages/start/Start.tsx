f1020ae99bc12833cfe5deff56b39d14ec3bbfa3
Julian-Kl <40470983+Julian-Kl@users.noreply.github.com>
2021-11-29 17:33:25 +0100

    startpage made nicer

--- src/app/pages/start/Start.tsx
+++ src/app/pages/start/Start.tsx
@@ -12,14 +12,19 @@ export const Start: React.FC = () => {
             </Helmet>
             <ParticlesBg type='cobweb' color='#1976d2' bg />
             <div style={{ position: 'absolute', top: 200, left: 80 }}>
-                <Grow in {...{ timeout: 1000 }}>
+                <Grow in {...{ timeout: 1500 }}>
                     <Typography variant='h1' component='h1'>
                         Synergic
                     </Typography>
                 </Grow>
-                <Grow in {...{ timeout: 2000 }}>
-                    <Typography variant='h2' component='h2'>
+                <Grow in {...{ timeout: 1800 }}>
+                    <Typography variant='h2' component='h2' color='primary'>
                         Atomic Design Page Builder
+                    </Typography>
+                </Grow>
+                <Grow in {...{ timeout: 2600 }}>
+                    <Typography variant='h5' component='p'>
+                        Bachelorthesis Project by Julian Klummer
                     </Typography>
                 </Grow>
             </div>
