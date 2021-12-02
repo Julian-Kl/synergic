55822784a74de5fbc62a54187aad6c52cc5d6452
Julian-Kl <julianklummer@outlook.de>
2021-12-02 09:38:34 +0100

    cleaned dependencies

--- src/client.tsx
+++ src/client.tsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from 'react'
 import { hydrate } from 'react-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
-import { getCLS, getFID, getLCP } from 'web-vitals'
 import { App } from './app/App'
 
 hydrate(
@@ -15,7 +14,3 @@ hydrate(
     </StrictMode>,
     document.getElementById('app')
 )
-
-getCLS(console.log)
-getFID(console.log)
-getLCP(console.log)
